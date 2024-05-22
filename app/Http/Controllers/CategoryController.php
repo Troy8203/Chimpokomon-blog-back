72fccd025f0378bd49f0bffc447205e0b4539c53
Fernando Nina <Fernandonina611@gmail.com>
2024-05-22 15:01:05 -0400

    feat: ✨agregado CRUD de categorias

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\CategoryCollection;
 use App\Models\Category;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CategoryController extends Controller
 {
@@ -13,6 +17,7 @@ class CategoryController extends Controller
     public function index()
     {
         //GET
+        return new CategoryCollection(Category::where( 'status', 'ACTIVE')->get());
     }
 
     /**
@@ -21,6 +26,42 @@ class CategoryController extends Controller
     public function store(Request $request)
     {
         //POST
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:100|unique:categories,name',
+            //el nomnbre debe ser unico
+            'description' => 'string|max:250',
+        ]);
+        //Errores de validacion
+        if ($validator->fails()) {
+            //retornamos un error
+            return response()->json([
+                'message' => 'Existen errores de validacion',
+                'errors' => $validator->errors(),
+                'status' => 422
+            ], 422);
+        }
+        //Creando la Categoria
+        $category = Category::create([
+            'name' => $request->name,
+            'description' => $request->descripction
+        ]);
+        //Si ocurrio un error la crear la Categoria
+        if (!$category) {
+            $data = [
+                'message' => 'error al crear la Categoria',
+                'status' => 500
+            ];
+            return response()->json($data, 500);
+        }
+
+        //Categoria creado con éxito
+
+        if ($category) {
+            return response()->json([
+                "message" => "Categoria registrado!",
+                "status" => 200
+            ], 200);
+        }
     }
 
     /**
@@ -29,7 +70,23 @@ class CategoryController extends Controller
     public function show(string $id)
     {
         //GET CON ID  /category/{id}
-
+        $category = Category::find($id);
+        if ($category) {
+            return response()->json([
+                'message' => 'Categoria encontrada..!',
+                'status' => 200,
+                'data' => [
+                    'name' => $category->name,
+                    'description' => $category->description,
+                    'estado' => $category->status
+                ]
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'Categoria no encontrado',
+                'status' => 404
+            ], 404);
+        }
     }
 
     /**
@@ -37,8 +94,58 @@ class CategoryController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //patch o put
+        //patch
+        if ($request->isMethod('put')) {
+            return response()->json([
+                'message' => 'El método PUT no está permitido en esta ruta, INTENTA CON PATCH :)',
+                'status' => 405
+            ], 405);
+        }
+        $category = Category::find($id);
+        if ($category) {
+            $validator = Validator::make($request->all(), [
+                'name' => 'string|max:100|unique:categories,name,' . $id . ',id',
+                'description' => 'string|max:250',
+            ]);
 
+            if ($validator->fails()) {
+                return response()->json([
+                    'message' => 'Existen errores de validación',
+                    'errors' => $validator->errors(),
+                    'status' => 422,
+                ], 422);
+            }
+            //Actualizando la categoriaa
+            try{
+            $respuesta = $category->update([
+                'name' => $request->name,
+                'description' => $request->description,
+            ]);
+        }catch (QueryException $e) {
+            // Capturar y manejar la excepción de la base de datos
+            return response()->json([
+                'message' => 'Ocurrió un error al actualizar: ' . $e->getMessage(),
+                'status' => 500
+            ], 500);
+        }
+
+            if ($respuesta) {
+                return response()->json([
+                    'message' => 'Categoria actualizado con éxito.!',
+                    'status' => 200
+                ], 200);
+            } else {
+                return response()->json([
+                    'message' => 'Ocurrió un error al actualizar los datos. :(',
+                    'status' => 422
+                ], 422);
+            }
+        } else {
+            return response()->json([
+                "message" => 'No se ha encontrado el registro',
+                "status" => 404
+            ], 404);
+        }
     }
 
     /**
@@ -47,5 +154,30 @@ class CategoryController extends Controller
     public function destroy(string $id)
     {
         //DELETE
+        //Cambiando el estado a inactivo
+        try {
+            // Buscar la categoria
+            $categoria = Category::findOrFail($id);
+
+            // Actualizar el estado a "INACTIVO"
+            $categoria->status = 'INACTIVE';
+            $categoria->save();
+            return response()->json([
+                'message' => 'Categoria inactivado con éxito',
+                'status' => 200
+            ], 200);
+        } catch (QueryException $e) {
+            // Capturar y manejar la excepción de la base de datos
+            return response()->json([
+                'message' => 'Ocurrió un error al actualizar el estado: ' . $e->getMessage(),
+                'status' => 500
+            ], 500);
+        } catch (ModelNotFoundException $e) {
+            // Manejar el caso donde no se encuentra el registro
+            return response()->json([
+                'message' => 'Categoria no encontrado en la BD',
+                'status' => 404
+            ], 404);
+        }
     }
 }
--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CategorySeeder extends Seeder
 {
@@ -12,6 +13,77 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('categories')->insert([
+            [
+                'name' => 'Tecnología',
+                'description' => 'Todo sobre lo último en tecnología y gadgets.',
+                'status' => 'ACTIVE',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Salud',
+                'description' => 'Consejos de salud, noticias e información.',
+                'status' => 'ACTIVE',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Estilo de Vida',
+                'description' => 'Artículos sobre estilo de vida, moda y bienestar.',
+                'status' => 'ACTIVE',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Viajes',
+                'description' => 'Guías e historias sobre viajar por el mundo.',
+                'status' => 'ACTIVE',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Finanzas',
+                'description' => 'Consejos para manejar el dinero y noticias financieras.',
+                'status' => 'ACTIVE',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Educación',
+                'description' => 'Recursos y noticias en el sector educativo.',
+                'status' => 'ACTIVE',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Entretenimiento',
+                'description' => 'Últimas noticias sobre películas, música y programas de TV.',
+                'status' => 'ACTIVE',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Deportes',
+                'description' => 'Actualizaciones y noticias en el mundo de los deportes.',
+                'status' => 'ACTIVE',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Ciencia',
+                'description' => 'Descubrimientos y noticias en el mundo de la ciencia.',
+                'status' => 'ACTIVE',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Comida',
+                'description' => 'Recetas, noticias de alimentos y consejos culinarios.',
+                'status' => 'ACTIVE',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     }
 }
