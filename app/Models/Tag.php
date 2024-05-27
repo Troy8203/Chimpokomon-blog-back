6df75d5c926f5781454bce59c64be5d545d04587
Fernando Nina <Fernandonina611@gmail.com>
2024-05-27 17:56:05 -0400

    feat: 💩agregado CRUD de tags
    Se agregaron los archivos `TagPolicy.php`, `TagCollection.php`, `TagResource.php`, `TagFactory.php`, `TagStoreRequest.php` y `TagUpdateRequest.php` en las respectivas carpetas del proyecto. Estos cambios permiten el manejo de tags en la aplicación, incluyendo políticas de acceso, transformación de recursos, generación de datos de prueba y validación de solicitudes.

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -8,7 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     use HasFactory;
-    protected $fillable = ['name'];
+    protected $fillable = [
+        'name',
+        'status'
+    ];
 
     /* Relacion de N a N Post tiene N tags*/
     public function posts()
