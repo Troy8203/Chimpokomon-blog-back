92d6eb6e2471c419ea38cb346d8897553ea6c27e
Fernando Nina <Fernandonina611@gmail.com>
2024-05-27 17:02:24 -0400

    feat: ✨ agregar filtro de API
    Se agregó el archivo `ApiFilter.php` en la carpeta `app/Filters` para implementar un filtro de API que transforma los parámetros de la solicitud en una consulta de Eloquent. Esto mejora la funcionalidad de la API al permitir filtrar los resultados según los parámetros proporcionados en la solicitud.

---
+++ app/Filters/ApiFilter.php
@@ -0,0 +1,34 @@
+<?php
+namespace App\Filters;
+use Illuminate\Http\Request;
+class ApiFilter{
+
+    protected $safeParams = [];
+    protected $columnMap = [];
+    protected $operatorMap = [];
+
+    public function transform(Request $request){
+        $eloQuery = [];
+        foreach($this->safeParams as $parm => $operators){
+            $query = $request->query($parm);
+
+            if(!isset($query)){
+                continue;
+            }
+
+            $column = $this->columnMap[$parm] ?? $parm;
+            foreach($operators as $operator){
+                if(isset($query[$operator])){
+                    //Capturamos el valor a evaluar
+                    $value = $query[$operator];
+                    // Si el operador es 'LIKE', añadir los comodines '%' al valor
+                    if ($operator === 'lk') {
+                        $value = "%{$value}%";
+                    }
+                    $eloQuery[] = [$column, $this->operatorMap[$operator],$value];
+                }
+            }
+        }
+        return $eloQuery;
+    }
+}
